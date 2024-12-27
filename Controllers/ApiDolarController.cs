7085cf87bd4ae6af851d5f8a7fd0be17ddb63f87
Epic <byepic@outlook.es>
2024-12-27 20:55:20 -0300

    Mostrar api sismo

--- Controllers/ApiDolarController.cs
+++ Controllers/ApiDolarController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Newtonsoft.Json.Linq;
 
 namespace ClaseNosePeroClase.Controllers
@@ -19,6 +20,34 @@ namespace ClaseNosePeroClase.Controllers
                         ViewBag.dolar = serie["valor"]?.ToString();
                     
                 
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Sismo() 
+        {
+            string apiurl = $"https://api.xor.cl/sismo/recent";
+
+            using (HttpClient newclient = new HttpClient())
+            {
+                JObject datossismo = JObject.Parse(await newclient.GetStringAsync(apiurl));
+
+                var events = datossismo["events"]!;
+
+                List<string> fechas = new List<string>();
+                List<string> ubicaciones = new List<string>();
+                List<string> magnitudes = new List<string>();
+
+                foreach (var evento in events)
+                {
+                    fechas.Add(evento["local_date"]?.ToString());
+                    ubicaciones.Add(evento["geo_reference"]?.ToString());
+                    magnitudes.Add(evento["magnitude"]!["value"]?.ToString());
+                }
+
+                ViewBag.fechas = fechas;
+                ViewBag.ubicaciones = ubicaciones;
+                ViewBag.magnitudes = magnitudes;
             }
             return View();
         }
