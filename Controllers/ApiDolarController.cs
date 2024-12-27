4fe30c2b015b248eb230c4708708f5991035be18
Epic <byepic@outlook.es>
2024-12-27 20:39:05 -0300

    Cambios en el controllador-View

--- Controllers/ApiDolarController.cs
+++ Controllers/ApiDolarController.cs
@@ -5,21 +5,20 @@ namespace ClaseNosePeroClase.Controllers
 {
     public class ApiDolarController : Controller
     {
+
         public async Task<IActionResult> ObtenerDolar()
         {
-            string apiurl = $"https://mindicador.cl/api";
+            string apiurl = $"https://mindicador.cl/api/dolar";
             using (HttpClient client = new HttpClient()) 
             {
                 JObject datosdolar = JObject.Parse(await client.GetStringAsync(apiurl));
-                if (datosdolar["serie"] != null && datosdolar["serie"]!.Any()) 
-                {
+                
                     var serie = datosdolar["serie"]![0];
-                    if (serie != null) 
-                    {
+                    
                         ViewBag.fecha = serie["fecha"]?.ToString();
                         ViewBag.dolar = serie["valor"]?.ToString();
-                    }
-                }
+                    
+                
             }
             return View();
         }
