c557f28aa04ee8dcd16d7d2d41fb07f32094d011
Epic <byepic@outlook.es>
2024-12-27 20:21:57 -0300

    Consumo de api

---
+++ Controllers/ApiDolarController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ClaseNosePeroClase.Controllers
+{
+    public class ApiDolarController : Controller
+    {
+        public async Task<IActionResult> ObtenerDolar()
+        {
+            string apiurl = $"https://mindicador.cl/api";
+            using (HttpClient client = new HttpClient()) 
+            {
+                JObject datosdolar = JObject.Parse(await client.GetStringAsync(apiurl));
+                if (datosdolar["serie"] != null && datosdolar["serie"]!.Any()) 
+                {
+                    var serie = datosdolar["serie"]![0];
+                    if (serie != null) 
+                    {
+                        ViewBag.fecha = serie["fecha"]?.ToString();
+                        ViewBag.dolar = serie["valor"]?.ToString();
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
